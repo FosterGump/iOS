97e66812be47817c80cbcbe7d1a833dcf91132dc
Hannah An <worm24d@gmail.com>
2016-02-01 22:59:54 -0600

    bien

--- JuegodeMemoria/MasterViewController.swift
+++ JuegodeMemoria/MasterViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class MasterViewController: UITableViewController {
 
     var detailViewController: DetailViewController? = nil
-    var objects = [AnyObject]()
+    var objects = [AnyObject]() 
 
 
     override func viewDidLoad() {
